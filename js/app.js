6ed2168017988543aaac82c4edd1765bc6b5b8dd
codeonium <codeon2020@gmail.com>
2021-04-01 16:17:25 +0100

    MVP completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+const form = document.querySelector('#new-item-form')
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const bookInfo = document.createElement('li');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const bookCategory = document.createElement('p');
+  bookTitle.textContent = event.target.title.value
+  bookAuthor.textContent = event.target.author.value
+  bookCategory.textContent = event.target.category.value
+  const list = document.querySelector('#reading-list');
+  const listElement = list.appendChild(bookInfo);
+  listElement.appendChild(bookTitle)
+  listElement.appendChild(bookAuthor)
+  listElement.appendChild(bookCategory)
+  document.getElementById('new-item-form').reset();
+
 })
+
+})
+
+
+
+    // // creating a new element
+    // const newListItem = document.createElement('li');
+    // newListItem.textContent = "Purple!!!";
+    // newListItem.classList.add("purple")
+    // //creating a place for it to go
+    // const list = document.querySelector('ul');
+    // list.appendChild(newListItem)
